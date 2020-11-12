18871fc23bdf4fbe0b46e169cb94d0120ba16283
Mateus Henrique <mateushenriquechaves1@gmail.com>
2020-11-12 11:35:33 -0300

    create home page tests

--- src/pages/Home/home.test.js
+++ src/pages/Home/home.test.js
@@ -3,10 +3,16 @@ import { shallow } from 'enzyme'
 import 'jest-styled-components'
 import Home from '.'
 import Button from '../../components/Button'
+import Body from '../../components/Body'
 
 describe('Testing Home Page', () => {
   it('should have two buttons on home screen', () => {
     const wrapper = shallow(<Home />)
     expect(wrapper.find(Button)).toHaveLength(2)
+  })
+
+  it('should have Body component on home screen', () => {
+    const wrapper = shallow(<Home />)
+    expect(wrapper.find(Body)).toHaveLength(1)
   })
 })
